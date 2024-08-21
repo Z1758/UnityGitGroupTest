aed3da62402d0cb7d94f3e44bc413f304ae8fb1e
ohjh1744 <65078015+ohjh1744@users.noreply.github.com>
2024-08-21 17:45:50 +0900

    OJH 스크립트 수정
    OJH 스크립트 Start에 한줄 추가

--- GitBranchTest/Assets/OJH/OJH.cs
+++ GitBranchTest/Assets/OJH/OJH.cs
@@ -7,7 +7,7 @@ public class OJH : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Debug.Log("Hi IM OJH");
     }
 
     // Update is called once per frame
